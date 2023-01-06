9ab283e48f24db1e3556428ca2ddf83b64376b3f
Asjal Abrar <asjalabrar8@gmail.com>
2023-01-06 17:50:54 +0500

    Backend Completed with Sanity IO

---
+++ backend/schemas/index.ts
@@ -0,0 +1,9 @@
+import testimonials from './testimonials'
+import about from './abouts'
+import brand from './brands'
+import contact from './contact'
+import skill from './skills'
+import workExperience from './workExperience'
+import portfolio from './portfolio'
+
+export const schemaTypes = [testimonials, about, brand, contact, skill, workExperience, portfolio]
